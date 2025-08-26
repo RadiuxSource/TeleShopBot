19f830c13d184930b1c7974571e8c2a1841d31d5
RadiuxSource <adityajsru17@gmail.com>
2025-08-26 22:51:46 +0530

    update

--- Modules/__main__.py
+++ Modules/__main__.py
@@ -1,32 +1,16 @@
 import asyncio
 import importlib
-from aiohttp import web
 from pyrogram import idle
 from Modules.plugins import ALL_MODULES
-from Modules import scheduler, db
-from Modules.setup_db import setup_indexes
+
 
 loop = asyncio.get_event_loop()
 
-async def index(request):
-    return web.Response(text='Hello from Telegram Bot!')
-
-async def read_logs(request):
-    try:
-        with open('zenova.log', 'r') as f:
-            return web.Response(text=f.read())
-    except FileNotFoundError:
-        return web.Response(status=404, text="Log file not found")
-    except Exception as e:
-        return web.Response(status=500, text=str(e))
-
 async def cbot_boot():
-    await setup_indexes(db)
     for all_module in ALL_MODULES:
         importlib.import_module("Modules.plugins." + all_module)
     print(str(ALL_MODULES))
     print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎lly 𝗌𝗍𝖺𝗋𝗍")
-    scheduler.start()
     await idle()
 
 if __name__ == "__main__":
